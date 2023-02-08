f67e292cfdf9c3e039423a388a03dd1ee504ddb4
desfeк <desfe@DESKTOP-86DPKL3>
2023-02-08 17:43:15 +0500

    lv

--- Main.cpp
+++ Main.cpp
@@ -3,29 +3,40 @@
 #include <algorithm>
 class Car {
 public:
-	explicit Car(size_t num = 0) : _size(num), 
-		_acceleration("Vwoom...vwoommm...vwooommm..."){log("constructor\n");}
+	explicit Car(size_t num = 0) : _size(num), _data(new int[_size]), _acceleration("\nVwoom...vwoommm...vwooommm...\n")
+	{log("constructor");}
 	
-	Car(const Car& other) : _size(other._size), _acceleration("Vwoom...vwoommm...vwooommm..."){
-		log("copy constructor\n");
+	~Car() {
+		log("Deconstructor");
+		if (_data) {
+			delete[] _data;
+			_data = nullptr;
+		}
+	}
+	Car(const Car& other) : _size(other._size), _data(new int[_size]), _acceleration("\nVwoom...vwoommm...vwooommm...\n")
+	{
+		log("copy constructor");
+		_acceleration = other._acceleration;
 		for (size_t i = 0; i < _size; i++) {
-			_acceleration[i] = other._acceleration[i];
+			_data = other._data;
 		}
 	}
 	
 	Car& operator=(const Car& other){
 		log("copy assigment operator");
 		Car tmp(other);
+		std::swap(_acceleration, tmp._acceleration);
 		std::swap(_size, tmp._size);
-		std::swap(_size, tmp._size);
+		std::swap(_data, tmp._data);
 		return *this;
 	}
 	
 	Car& operator=(Car&& other) noexcept {
 		log("move assigment operation");
+		std::swap(_acceleration, other._acceleration);
 		std::swap(_size, other._size);
-		std::swap(_size, other._size);
-		return*this;
+		std::swap(_data, other._data);
+		return *this;
 	}
 	
 	void push_acceleration() {
@@ -35,18 +46,21 @@ public:
 private:
 	
 	void log(const char* msg) {
-		std::cout << "[" << this << "]" << msg<< "\n";
+		std::cout << "[" << this << "] - " << msg<< "\n";
 	}
 	std::string _acceleration;
 	size_t _size;
+	int* _data;
 };
 int main() {
-	Car v1(40);
+	Car v1(10);
 	Car v2;
 	Car v3;
 	v1.push_acceleration();
-	std::cout << "++++++++++++++++++++++++" << std::endl;
 	v2 = v1;
+	v2.push_acceleration();
 	v3 = std::move(v1);
+	v3 = Car(20);
+	v3.push_acceleration();
 	return 0;
 }
